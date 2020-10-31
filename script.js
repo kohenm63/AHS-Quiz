ce2be0e2a7a000cd00ea44ebd634e41cb4b690f0
Mila <48856476+kohenm63@users.noreply.github.com>
2020-10-31 11:36:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.display = 'flex';
+        setTimeout(() =>main.style.opacity = 1, 50);
+        },4000);
+}
+
+init();
+
 const quizData = [
     {
         question: " What is the most used programming language in 2020?",
